a6d960fadd77d11c5029442e60c86263ac129786
明月 <370752206@qq.com>
2025-03-30 21:04:58 +0800

    修改描述

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 import webpack from "next/dist/compiled/webpack/webpack-lib.js";
-import crypto from "crypto-browserify";
-import stream from "stream-browserify";
-import buffer from "buffer";
-import path from "path-browserify";
-import os from "os-browserify/browser.js";
 
 let userConfig = undefined;
 try {
@@ -32,18 +27,12 @@ const nextConfig = {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        crypto: crypto.resolve ? crypto.resolve : false,
-        stream: stream.resolve ? stream.resolve : false,
-        buffer: buffer.resolve ? buffer.resolve : false,
-        path: path.resolve ? path.resolve : false,
-        os: os.resolve ? os.resolve : false,
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer/"),
         fs: false,
         http: false,
         https: false,
-        zlib: false,
-        net: false,
-        tls: false,
-        child_process: false,
       };
 
       config.plugins.push(
