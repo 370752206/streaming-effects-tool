de87de3edec9e9989300e474e09e10986716fcef
明月 <370752206@qq.com>
2025-03-30 21:07:56 +0800

    修改描述

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 import webpack from "next/dist/compiled/webpack/webpack-lib.js";
 
-let userConfig = undefined;
-try {
-  userConfig = await import("./v0-user-next.config");
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -17,11 +10,6 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
@@ -45,27 +33,5 @@ const nextConfig = {
     return config;
   },
 };
-
-mergeConfig(nextConfig, userConfig);
-
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return;
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === "object" &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      };
-    } else {
-      nextConfig[key] = userConfig[key];
-    }
-  }
-}
 
 export default nextConfig;
